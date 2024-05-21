4c8a6e672334a1f270aac576a096542249533a01
Fremy Santana <frmysantana@yahoo.com>
2024-05-20 21:45:06 -0400

    initial setup and script for scraping taco bell

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cheatMealGenerator
+
+Personal project to see if I can build something that can come up with cheat meal recommendations for any restaurant (that has calorie amounts for its food) that would remain below an upper bound.
---
+++ src/tacoBellScrapper.ts
@@ -0,0 +1,15 @@
+import cheerio from "npm:cheerio";
+
+export default async function tacoBellNutritionScrapper(req): Promise<Response> {
+  const sourceUrl = `https://www.nutritionix.com/taco-bell/menu/premium`;
+  const siteText = await fetch(sourceUrl);
+  const $ = cheerio.load(await siteText.text());
+  const items = $("tr.odd, tr.even").map((i, e) => {
+    const name = $(e).find(".nmItem").html();
+    const calories = $(e).find("[aria-label*='Calories']").html();
+
+    return { name: name, calories: calories };
+  }).toArray();
+
+  return Response.json({ items });
+}
