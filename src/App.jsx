eddc864782aa9bdc3e8c36bb0bc4a42918e4313c
Fremy Santana <frmysantana@yahoo.com>
2024-06-12 22:49:26 -0400

    reactivity in the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,46 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [ calorieLimit, setCalorieLimit ] = useState(100);
+  const [ error, setError ] = useState('');
+
+  const submitLimit = () => {
+    console.log(`should be submitting... ${calorieLimit}`)
+    if (calorieLimit) {
+      fetch(`/meals?limit=${calorieLimit}`)
+    }
+  }
 
   return (
     <>
       <h1>Cheat Meal Generator</h1>
       <div className="card">
         <p>Please enter an upper calorie bound for your cheat meal (the maximum is 2000).</p>
-        <input id="calorie-bound" type="number" min={100} max={2000}></input>
+        <div>
+          <div>
+            <input id="calorie-bound" type="number" min={100} max={2000} 
+              onChange={(e) => {
+                const number = +e.target.value;
+                console.log({e})
+                if (number < 100) {
+                  setError("Calorie limit must be at least 100.")
+                  return
+                }
+
+                if (number > 2000) {
+                  setError("Calorie limit cannot exceed 2000.")
+                  return
+                }
+
+                setError('')
+                setCalorieLimit(number)
+              }}
+              value={calorieLimit}
+            />
+            <button onClick={submitLimit}>Generate</button>
+          </div>
+        <p>{error}</p>
+        </div>
       </div>
     </>
   )
